edeb93f29322979df5086c4960c8d3f7f6e6562a
Elais <e.papa@live.de>
2024-11-01 21:16:17 +0100

    feat: Add theme library with initial configuration and setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,8 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
 import { provideRouter } from '@angular/router';
+import { provideColorThemes } from '@ethlete/theming';
+import { THEMES } from '@lamentis/theme';
 import { environment } from '../environments/environment';
 import { routes } from './app.routes';
 
@@ -11,6 +13,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideColorThemes(THEMES),
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth()),
     provideAnalytics(() => getAnalytics()),
