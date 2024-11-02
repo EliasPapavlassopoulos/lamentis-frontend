069d755eb33e78310a93f156496e833fc48916dc
Elais <e.papa@live.de>
2024-11-02 15:09:56 +0100

    feat: Add color theme provider to application configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideColorThemes } from '@ethlete/theming';
+import { THEMES } from '../../theme';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes),
+        provideColorThemes(THEMES),
     ],
 };
