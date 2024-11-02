b70e4e4dbfec577e05154fb2a5ba79affa2e2566
Elais <e.papa@live.de>
2024-11-02 13:56:14 +0100

    feat: Migrate PostCSS and Tailwind configuration to TypeScript; remove old config files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss';
+
+export default {
     content: ['./src/**/*.{html,ts,scss}'],
     theme: {
         extend: {},
     },
     plugins: [],
-};
+} satisfies Config;
