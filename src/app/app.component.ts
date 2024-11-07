4d6508ca4dbca451c7b5205cee1edab7ed260e1f
Elais <e.papa@live.de>
2024-11-07 17:35:09 +0100

    refactor: Remove unused authentication logic and simplify AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AuthService } from './core/services';
 
 @Component({
     selector: 'lamentis-root',
@@ -10,20 +9,7 @@ import { AuthService } from './core/services';
         <p class="text-pretty pt-[20rem] text-center text-[3rem]">
             See you soon !
         </p>
-
-        <button (click)="register('e.papa@live.de', '12345678')">
-            RegisterUser
-        </button>
-
         <router-outlet />
     `,
 })
-export class AppComponent {
-    #authServeice = inject(AuthService);
-
-    register(email: string, password: string) {
-        if (email && password) {
-            this.#authServeice.register(email, password);
-        }
-    }
-}
+export class AppComponent {}
